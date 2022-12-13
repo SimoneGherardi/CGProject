09551705da689413d81ac9befd2e67dbecfb2db8
gmasini97 <20663565+gmasini97@users.noreply.github.com>
2022-12-14 00:28:44 +0100

    added reactphysics3d library

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@
 
 #include <chrono>
 
+#include <reactphysics3d/reactphysics3d.h>
+
 #define STB_IMAGE_IMPLEMENTATION
 
 
@@ -1455,9 +1457,43 @@ private:
     }
 };
 
+using namespace reactphysics3d;
 int main()
 {
-    CGProject app;
+    // First you need to create the PhysicsCommon object. 
+    // This is a factory module that you can use to create physics 
+    // world and other objects. It is also responsible for 
+    // logging and memory management 
+    PhysicsCommon physicsCommon;
+
+    // Create a physics world 
+    PhysicsWorld* world = physicsCommon.createPhysicsWorld();
+
+    // Create a rigid body in the world 
+    Vector3 position(0, 20, 0);
+    Quaternion orientation = Quaternion::identity();
+    Transform transform(position, orientation);
+    RigidBody* body = world->createRigidBody(transform);
+
+    const decimal timeStep = 1.0f / 60.0f;
+
+    // Step the simulation a few steps 
+    for (int i = 0; i < 20; i++) {
+
+        world->update(timeStep);
+
+        // Get the updated position of the body 
+        const Transform& transform = body->getTransform();
+        const Vector3& position = transform.getPosition();
+
+        // Display the position of the body 
+        std::cout << "Body Position: (" << position.x << ", " <<
+            position.y << ", " << position.z << ")" << std::endl;
+    }
+
+    return 0;
+    
+    /*CGProject app;
 
     try
     {
@@ -1469,5 +1505,6 @@ int main()
         return EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS;*/
+    
 }

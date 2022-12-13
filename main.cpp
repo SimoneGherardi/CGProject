c7fcbabc591e80e06f50eade86730027f57f4587
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2022-12-14 00:15:10 +0100

    added flecs library dependencies

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@
 #include <chrono>
 
 #include <reactphysics3d/reactphysics3d.h>
+#include <flecs.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 
@@ -1458,18 +1459,13 @@ private:
 };
 
 using namespace reactphysics3d;
-int main()
+
+void testPhysics()
 {
-    // First you need to create the PhysicsCommon object. 
-    // This is a factory module that you can use to create physics 
-    // world and other objects. It is also responsible for 
-    // logging and memory management 
     PhysicsCommon physicsCommon;
 
-    // Create a physics world 
     PhysicsWorld* world = physicsCommon.createPhysicsWorld();
 
-    // Create a rigid body in the world 
     Vector3 position(0, 20, 0);
     Quaternion orientation = Quaternion::identity();
     Transform transform(position, orientation);
@@ -1477,23 +1473,25 @@ int main()
 
     const decimal timeStep = 1.0f / 60.0f;
 
-    // Step the simulation a few steps 
     for (int i = 0; i < 20; i++) {
 
         world->update(timeStep);
 
-        // Get the updated position of the body 
         const Transform& transform = body->getTransform();
         const Vector3& position = transform.getPosition();
 
-        // Display the position of the body 
         std::cout << "Body Position: (" << position.x << ", " <<
             position.y << ", " << position.z << ")" << std::endl;
     }
 
     return 0;
+}
+
+int main()
+{
+    testPhysics();
     
-    /*CGProject app;
+    CGProject app;
 
     try
     {
@@ -1505,6 +1503,6 @@ int main()
         return EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;*/
+    return EXIT_SUCCESS;
     
 }

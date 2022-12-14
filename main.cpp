a1c974cf15a4d681223436e0d2d7590be487fd08
SimoneGherardi <simo95omis@gmail.com>
2022-12-14 15:32:44 +0100

    createCommandBuffers()

--- main.cpp
+++ main.cpp
@@ -402,6 +402,11 @@ private:
     VkImageView colorImageView;
 
     std::vector<VkFramebuffer> swapChainFramebuffers;
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::vector<VkSemaphore> imageAvailableSemaphores;
+    std::vector<VkSemaphore> renderFinishedSemaphores;
+    std::vector<VkFence> inFlightFences;
+    std::vector<VkFence> imagesInFlight;
     size_t currentFrame = 0;
 
     VkDescriptorPool descriptorPool;
@@ -449,6 +454,13 @@ private:
     
     std::vector<SceneModel> SkyBox;
 
+    // Camera
+    glm::vec3 CamAng = glm::vec3(0.0f);
+    glm::vec3 CamPos = glm::vec3(0.0f, 0.5f, 5.0f);
+
+    // Other global variables
+    int curText = 0;
+
     void initWindow() {
 
         glfwInit();
@@ -478,7 +490,7 @@ private:
         createDescriptorPool();
         createDescriptorSets();
 
-        createCommandBuffer();
+        createCommandBuffers();
 
     }
    
@@ -968,15 +980,86 @@ private:
         }
     }
 
-    void createCommandBuffer() {
+    void createCommandBuffers() {
+        commandBuffers.resize(swapChainFramebuffers.size());
+
         VkCommandBufferAllocateInfo allocInfo{};
         allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
         allocInfo.commandPool = commandPool;
         allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-        allocInfo.commandBufferCount = 1;
+        allocInfo.commandBufferCount = (uint32_t)commandBuffers.size();
 
-        if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS) {
+        VkResult result = vkAllocateCommandBuffers(device, &allocInfo,
+            commandBuffers.data());
+        if (result != VK_SUCCESS) {
+            PrintVkError(result);
             throw std::runtime_error("failed to allocate command buffers!");
+        }
+
+        for (size_t i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+            beginInfo.flags = 0; // Optional
+            beginInfo.pInheritanceInfo = nullptr; // Optional
+
+            if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) !=
+                VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = renderPass;
+            renderPassInfo.framebuffer = swapChainFramebuffers[i];
+            renderPassInfo.renderArea.offset = { 0, 0 };
+            renderPassInfo.renderArea.extent = swapChainExtent;
+
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = { 0.0f, 0.0f, 0.0f, 1.0f };
+            clearValues[1].depthStencil = { 1.0f, 0 };
+
+            renderPassInfo.clearValueCount =
+                static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo,
+                VK_SUBPASS_CONTENTS_INLINE);
+
+            vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS,
+                PhongPipeline);
+            //			for(int j = 0; j < Scene.size(); j++) {
+            {int j = curText;
+            VkBuffer vertexBuffers[] = { Scene[j].MD.vertexBuffer };
+            VkDeviceSize offsets[] = { 0 };
+            vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
+            vkCmdBindIndexBuffer(commandBuffers[i], Scene[j].MD.indexBuffer, 0,
+                VK_INDEX_TYPE_UINT32);
+            vkCmdBindDescriptorSets(commandBuffers[i],
+                VK_PIPELINE_BIND_POINT_GRAPHICS,
+                PhongPipelineLayout, 0, 1,
+                &PhongDescriptorSets[i + j * swapChainImages.size()],
+                0, nullptr);
+
+            vkCmdDrawIndexed(commandBuffers[i],
+                static_cast<uint32_t>(Scene[j].MD.indices.size()), 1, 0, 0, 0);
+            }
+
+            // Draws the Skybox
+            vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS,
+                SkyBoxPipeline);
+            VkBuffer vertexBuffers[] = { SkyBox[curText].MD.vertexBuffer };
+            VkDeviceSize offsets[] = { 0 };
+            vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
+            vkCmdBindIndexBuffer(commandBuffers[i], SkyBox[curText].MD.indexBuffer, 0,
+                VK_INDEX_TYPE_UINT32);
+            vkCmdBindDescriptorSets(commandBuffers[i],
+                VK_PIPELINE_BIND_POINT_GRAPHICS,
+                SkyBoxPipelineLayout, 0, 1,
+                &SkyBoxDescriptorSets[i + curText * swapChainImages.size()],
+                0, nullptr);
+            vkCmdDrawIndexed(commandBuffers[i],
+                static_cast<uint32_t>(SkyBox[curText].MD.indices.size()), 1, 0, 0, 0);
+
         }
     }
 

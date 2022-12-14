aa26682084d4d8747d052fdcf4f9ba86735ba1a2
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2022-12-14 04:29:53 +0100

    added case with single invocation

--- main.cpp
+++ main.cpp
@@ -1496,8 +1496,15 @@ void testECS()
     flecs::world world;
     world.import<Test>();
 
-    auto e = world.entity()
+    auto a = world.entity()
         .set<Counter>({ 0 });
+    auto b = world.entity()
+        .set<Counter>({ 10 });
+    auto c = world.entity()
+        .set<Counter>({ 20 });
+    world.system<Counter>().kind(flecs::OnUpdate).iter([](flecs::iter it) {
+        printf("AAA\n");
+        });
 
     world.progress();
     world.progress();

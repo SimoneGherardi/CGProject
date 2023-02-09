883a82689c51277261ccbba4017e69e80a048cea
gmasini97 <20663565+gmasini97@users.noreply.github.com>
2023-02-09 23:45:16 +0100

    static buffer allocator (?)

--- Allocator.cpp
+++ Allocator.cpp
@@ -1,23 +1,43 @@
 #include "Allocator.h"
 
+//uint32_t findMemoryType(
+//    VkPhysicalDevice physicalDevice,
+//    uint32_t typeFilter,
+//    VkMemoryPropertyFlags properties
+//)
+//{
+//    VkPhysicalDeviceMemoryProperties memProperties;
+//    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+//
+//    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+//        if ((typeFilter & (1 << i)) &&
+//            (memProperties.memoryTypes[i].propertyFlags & properties) ==
+//            properties) {
+//            return i;
+//        }
+//    }
+//
+//    throw std::runtime_error("failed to find suitable memory type!");
+//}
 uint32_t findMemoryType(
-    VkPhysicalDevice physicalDevice,
-    uint32_t typeFilter,
-    VkMemoryPropertyFlags properties
+	VkPhysicalDevice physicalDevice,
+	VkMemoryPropertyFlags properties,
+	VkDeviceSize minimumHeapSize
 )
 {
-    VkPhysicalDeviceMemoryProperties memProperties;
-    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+	VkPhysicalDeviceMemoryProperties memProperties;
+	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
 
-    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
-        if ((typeFilter & (1 << i)) &&
-            (memProperties.memoryTypes[i].propertyFlags & properties) ==
-            properties) {
-            return i;
-        }
-    }
-
-    throw std::runtime_error("failed to find suitable memory type!");
+	for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+		VkMemoryType t = memProperties.memoryTypes[i];
+		bool okProperties = (t.propertyFlags & properties) == properties;
+		if (okProperties) {
+			bool okHeap = memProperties.memoryHeaps[t.heapIndex].size >= minimumHeapSize;
+			if (okHeap) {
+				return i;
+			}
+		}
+	}
 }
 
 StaticBufferAllocator::StaticBufferAllocator(
@@ -26,15 +46,20 @@ StaticBufferAllocator::StaticBufferAllocator(
 	const VkDeviceSize size
 )
 {
-    VkMemoryRequirements memoryRequirements = {};
-    memoryRequirements.alignment = 16; // ?
-    memoryRequirements.memoryTypeBits = 
-
 	VkMemoryAllocateInfo memoryAllocateInfo = {};
 	memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
 	memoryAllocateInfo.allocationSize = size;
 	memoryAllocateInfo.pNext = nullptr;
     memoryAllocateInfo.memoryTypeIndex = findMemoryType(
+        physicalDevice,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+		size
+    );
 
-    )
+	CheckVkResult(vkAllocateMemory(
+		device,
+		&memoryAllocateInfo,
+		nullptr,
+		&DeviceMemory
+	));
 }

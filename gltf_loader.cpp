421eeee5d4b7a411db689d84af975c25f113ff7c
SimoneGherardi <simo95omis@gmail.com>
2023-03-30 15:21:03 +0200

    DeleteUI

--- gltf_loader.cpp
+++ gltf_loader.cpp
@@ -520,6 +520,7 @@ void loadDataFromGLTF(const char* fileName){
     std::string warn, err;
     bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, fileName);
 
+
     if (!ret) {
         std::cerr << err << std::endl;
         return;

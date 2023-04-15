7cae7cfd18214f259391c8abd2e9a8bf534ea3fa
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2023-04-15 18:18:32 +0200

    Added is trigger and removed dead code

--- physics.h
+++ physics.h
@@ -5,7 +5,6 @@
 typedef struct CollisionBody
 {
     rp3d::CollisionBody* Body;
-    // CollisionBody() : Body(NULL) {}
 } CollisionBody;
 
 typedef struct RigidBody
@@ -13,15 +12,14 @@ typedef struct RigidBody
     float Weight;
     rp3d::BodyType Type;
     rp3d::RigidBody* Body;
-    // RigidBody() : Body(NULL), Weight(70.f), Type(rp3d::BodyType::DYNAMIC) {}
 } RigidBody;
 
 typedef struct Collider
 {
     rp3d::Vector3 Size;
     rp3d::CollisionShapeName Type;
+    bool isTrigger;
     rp3d::Collider* RP3DCollider;
-    // Collider() : RP3DCollider(NULL), Size({1, 1, 1}), Type(rp3d::CollisionShapeName::CAPSULE) {}
 } Collider;
 
 class Physics
